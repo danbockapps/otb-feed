6ae72840d7c6a9b1cad2128dd1a446eae22032ba
Dan Bock <danbock@gmail.com>
2023-05-13 18:06:57 -0400

    Read from URL params

--- src/App.tsx
+++ src/App.tsx
@@ -49,51 +49,53 @@ const reducer = (state: IEvent[], action: Action) => {
 
 function App() {
   const [state, dispatch] = useReducer(reducer, [])
-  console.log(state.length)
 
   useEffect(() => {
-    ;['12911649', '12663913', '30094337'].forEach(n =>
-      fetch(`https://cors-anywhere.herokuapp.com/http://www.uschess.org/msa/MbrDtlTnmtHst.php?${n}`)
-        .then(r => r.text())
-        .then(t => {
-          const parser = new DOMParser()
-          const doc = parser.parseFromString(t, 'text/html')
-          const tables = doc.getElementsByTagName('table')
-          const idAndName = tables[3].getElementsByTagName('b')[0].innerText
+    new URLSearchParams(window.location.search)
+      .get('players')
+      ?.split(',')
+      .forEach(n =>
+        fetch(
+          `https://cors-anywhere.herokuapp.com/http://www.uschess.org/msa/MbrDtlTnmtHst.php?${n}`,
+        )
+          .then(r => r.text())
+          .then(t => {
+            const parser = new DOMParser()
+            const doc = parser.parseFromString(t, 'text/html')
+            const tables = doc.getElementsByTagName('table')
+            const idAndName = tables[3].getElementsByTagName('b')[0].innerText
 
-          const events: IEvent[] = Array.from(tables[6].getElementsByTagName('tr'))
-            .slice(2)
-            .map(tr => {
-              const [dateAndId, eventAndSection, reg, quick, blitz] = Array.from(
-                tr.getElementsByTagName('td'),
-              )
+            const events: IEvent[] = Array.from(tables[6].getElementsByTagName('tr'))
+              .slice(2)
+              .map(tr => {
+                const [dateAndId, eventAndSection, reg, quick, blitz] = Array.from(
+                  tr.getElementsByTagName('td'),
+                )
 
-              return {
-                info: {
-                  date: getUpperRow(dateAndId.innerHTML),
-                  id: getSmallText(dateAndId.innerHTML),
-                  name: eventAndSection.innerHTML.substring(
-                    eventAndSection.innerHTML.indexOf('>') + 1,
-                    eventAndSection.innerHTML.indexOf('<', 1),
-                  ),
-                },
-                performances: [
-                  {
-                    name: toUpperCamelCase(idAndName.split(':')[1].trim()),
-                    section: getSmallText(eventAndSection.innerHTML),
-                    reg: reg.innerHTML,
-                    quick: quick.innerHTML,
-                    blitz: blitz.innerHTML,
+                return {
+                  info: {
+                    date: getUpperRow(dateAndId.innerHTML),
+                    id: getSmallText(dateAndId.innerHTML),
+                    name: eventAndSection.innerHTML.substring(
+                      eventAndSection.innerHTML.indexOf('>') + 1,
+                      eventAndSection.innerHTML.indexOf('<', 1),
+                    ),
                   },
-                ],
-              }
-            })
+                  performances: [
+                    {
+                      name: toUpperCamelCase(idAndName.split(':')[1].trim()),
+                      section: getSmallText(eventAndSection.innerHTML),
+                      reg: reg.innerHTML,
+                      quick: quick.innerHTML,
+                      blitz: blitz.innerHTML,
+                    },
+                  ],
+                }
+              })
 
-          console.log({ playerEvents: events })
-
-          dispatch({ type: 'ADD_PERFORMANCES', payload: events })
-        }),
-    )
+            dispatch({ type: 'ADD_PERFORMANCES', payload: events })
+          }),
+      )
   }, [])
 
   return (
